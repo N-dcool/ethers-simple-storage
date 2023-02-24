a5a2e231cd668203541a0071da41ddb2db45de7e
NARESH CHOUDHARY <nareshnc82643@gmail.com>
2023-02-24 15:56:55 +0530

    created a Typescript version 🚀

--- deploy.ts
+++ deploy.ts
@@ -1,15 +1,15 @@
-const ethers = require("ethers");
-const fs = new require("fs-extra");
-require("dotenv").config();
+import {ethers} from "ethers";
+import * as fs from "fs-extra";
+import "dotenv/config"
 
 async function main() {
     // HTTP://127.0.0.1:7545
-    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL!);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
     // const encryptedJson = fs.readFileSync("./.encryptedKey.json");
     // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
     //     encryptedJson,
-    //     process.env.PRIVATE_PASSWORD
+    //     process.env.PRIVATE_PASSWORD!
     // );
     // wallet = await wallet.connect(provider);
     const abi = fs.readFileSync(
--- encryptKey.ts
+++ encryptKey.ts
@@ -1,12 +1,12 @@
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require("dotenv").config();
+import {ethers} from "ethers";
+import * as fs from "fs-extra";
+import "dotenv/config"
 
 async function main() {
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!);
   const encryptedJsonKey = await wallet.encrypt(
-    process.env.PRIVATE_PASSWORD,
-    process.env.PRIVATE_KEY
+    process.env.PRIVATE_PASSWORD!,
+    process.env.PRIVATE_KEY!
   );
   //console.log(encryptedJsonKey);
   fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
